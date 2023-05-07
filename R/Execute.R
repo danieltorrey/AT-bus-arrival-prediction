eca54b286b1d2f1fb2a757cbf10f1b6dbb3bb6bd
Daniel Torrey <daniel_torrey@hotmail.com>
2023-05-07 21:59:39 +1200

    Updated Execute.R

--- R/Execute.R
+++ R/Execute.R
@@ -1,11 +1,11 @@
 source(paste0(getwd(), '/R/Initialise.R'))
 
-source(paste0(dir_scripts, 'GetBusInfoData.R'))
-source(paste0(dir_scripts, 'GetSuburbData.R'))
-source(paste0(dir_scripts, 'GetWeatherData.R'))
+source(paste0(dir_scripts, '/GetBusInfoData.R'))
+source(paste0(dir_scripts, '/GetSuburbData.R'))
+source(paste0(dir_scripts, '/GetWeatherData.R'))
 
-source(paste0(dir_scripts, 'BusDataCleansing.R'))
-source(paste0(dir_scripts, 'WeatherDataCleansing.R'))
+source(paste0(dir_scripts, '/BusDataCleansing.R'))
+source(paste0(dir_scripts, '/WeatherDataCleansing.R'))
 
-source(paste0(dir_scripts, 'CombineAllData.R'))
-source(paste0(dir_scripts, 'DataExploration.R'))
+source(paste0(dir_scripts, '/CombineAllData.R'))
+source(paste0(dir_scripts, '/DataExploration.R'))
