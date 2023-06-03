48c05142beb410d4464b56ab883d3c9c7c7d2831
leonardophu <129243708+leonardophu@users.noreply.github.com>
2023-06-03 18:29:39 +1200

    Implemented XGBoost -> Adding Training/Test split

--- R/models/XGBoost.R
+++ R/models/XGBoost.R
@@ -6,36 +6,36 @@ library(ggplot2)
 
 # Preprocessing 
 
+set.seed(500)
 
-set.seed(1)
-
-
-model <- full_processed_data %>% 
-  select(c(route_short_name, 
-           stop_sequence, 
-           stop_lat, 
-           stop_lon, 
-           delay, 
-           dow=day_of_week, 
-           hod=time_frame, 
-           suburb, 
-           weather)) %>%
-  filter(delay < 10000 & delay > -10000) %>% 
+# We will consider a bus on time if no earlier and no later than 3 minutes 
+model_buses <- full_processed_data %>% 
+  select(c(route_short_name, stop_sequence, stop_lat, stop_lon, delay, dow=day_of_week, 
+           hod=time_frame, suburb, weather)) %>%
+  mutate(route_short_name = as.factor(route_short_name),
+         suburb=as.factor(suburb),
+         weather=as.factor(weather)) %>%
+  filter(delay > -10000 & delay < 10000) %>%
   mutate(on_time = if_else(delay > -180 & delay < 180, TRUE, FALSE)) %>% 
-  mutate(work_day =! dow %in% c('Sat', 'Sun')) %>%
-  mutate(peak_hours = work_day & (hod %in% c(6:8,16:18))) %>%
-  mutate(weather = as.factor(weather)) %>% 
-  mutate(suburb = as.factor(suburb)) %>%
+  mutate(work_day = !(dow %in% c('Sat', 'Sun'))) %>%
+  mutate(peak_hours = work_day & (hod %in% c(6:8, 16:18))) %>%
   na.omit() 
 
-# Checking for imbalanced data - not too imbalanced
-table(model$on_time)
+# Checking for imbalanced data
+table(model_buses$on_time)
+
+# Partitioning data into training and testing sets (80/20 split)
+train_idx <- sample(nrow(model_buses), 0.8*nrow(model_buses))
+train_buses <- model_buses[train_idx,]
+test_buses <- model_buses[-train_idx,]
 
 # Create a sparse matrix, we do not want delay and on time in this for our model
-sparse_matrix <- sparse.model.matrix(~ . - 1, data = model %>% select(-on_time, -delay))
+sparse_matrix <- sparse.model.matrix(~ . - 1, data = train_buses %>% select(-on_time, -delay))
+sparse_matrix_test <- sparse.model.matrix(~ . - 1, data = test_buses %>% select(-on_time, -delay))
 
 # Labels used for prediction and XGBoost
-actual = model$on_time
+actual = train_buses$on_time
+actual_test = test_buses$on_time
 
 # Cross Validation 
 
@@ -72,13 +72,13 @@ ggplot(results, aes(x = Rounds)) +
   ggtitle("XGBoost Cross Validation Error")
 
 
-# We choose depth of 4. We choose Round 7 then subtract Test Error Std approx 0.358. Largest Train Error that is smaller than 0.358 is Round 4 test error. So we choose 4 rounds
+# Here could either opt in for 3 if we using minimum or 2 using Test_Error + 1 Test_Error_Std. I opted in for minimum.
 xg_model <- xgboost(data = sparse_matrix,
                 label = actual,
                 max_depth=2, 
                 eta=0.1, 
                 nthread=2, 
-                nrounds = 4,
+                nrounds = 3,
                 objective = 'binary:logistic')
 
 
@@ -86,28 +86,28 @@ xg_model <- xgboost(data = sparse_matrix,
 xgb.plot.tree(model = xg_model)
 
 #Predicting our whole dataset
-predicted = predict(xg_model, sparse_matrix) 
-predicted = ifelse(predicted > 0.5, TRUE, FALSE)
-confuse_table = table(actual, predicted)
+XG_predicted = predict(xg_model, sparse_matrix_test) 
+XG_predicted = ifelse(XG_predicted > 0.5, TRUE, FALSE)
+XG_confuse_table = table(actual_test, XG_predicted)
 
 #Print the confusion table
-print(confuse_table)
+print(XG_confuse_table)
 
 # Compute TN, FN, FP, TP
-TN <- confuse_table[1]
-FP <- confuse_table[2]
-FN <- confuse_table[3]
-TP <- confuse_table[4]
+XG_TN <- XG_confuse_table[1]
+XG_FP <- XG_confuse_table[2]
+XG_FN <- XG_confuse_table[3]
+XG_TP <- XG_confuse_table[4]
 
 # Compute accuracy, precision, recall, and specificity
-accuracy <- (TN + TP) / sum(confuse_table)
-precision <- TP / (FP + TP)
-recall <- TP / (FN + TP)
-specificity <- TN / (TN + FP)
+XG_accuracy <- (XG_TN + XG_TP) / sum(XG_confuse_table)
+XG_precision <- XG_TP / (XG_FP + XG_TP)
+XG_recall <- XG_TP / (XG_FN + XG_TP)
+XG_specificity <- XG_TN / (XG_TN + XG_FP)
 
 # Create a table with the results
-results_table <- data.frame(Metric = c("Accuracy", "Precision", "Recall", "Specificity"),
-                            Value = c(accuracy, precision, recall, specificity))
+XG_results_table <- data.frame(Metric = c("Accuracy", "Precision", "Recall", "Specificity"),
+                            Value = c(XG_accuracy, XG_precision, XG_recall, XG_specificity))
 
 # Print the results table
-print(results_table)
+print(XG_results_table)
